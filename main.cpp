bb2314598f5bf22d3bc836d8caa73fb37458acf4
rmaltais <redgemalt@gmail.com>
2022-05-28 09:35:41 +0000

    Revision

--- main.cpp
+++ main.cpp
@@ -1,60 +1,34 @@
-/*
- *  Copyright (c) 2012 Neal Horman - http://www.wanlink.com
- *
- *  License: MIT open source (http://opensource.org/licenses/MIT)
- *      Summary;
- *      Use / modify / distribute / publish it how you want and
- *      if you use it, or don't, you can't hold me liable for how
- *      it does or doesn't work.
- *      If it doesn't work how you want, don't use it, or change
- *      it so that it does work.
- *
- *   Modifié par Réjean Maltais 2022-05-23
- *   Pour Oled SSD1306 128 x 64 I2C pas de reset pin (fake pin comme mentionné)
- */
-
 #include "Adafruit_SSD1306.h"
+#include "DS18B20.h"
 #include "mbed.h"
 
+//  Configuration pour le BluePill
 
+DigitalOut myLed(PC_13);
+I2C i2c(PB_9, PB_8); // Identification préalable
+Adafruit_SSD1306_I2c oled(i2c, PA_4, 0x78, 64, 128); // Indique mon ecran actuel 128x64 - PA_4 (Fake pin)
 
-// an I2C sub-class that provides a constructed default
-class I2CPreInit : public I2C {
-public:
-  I2CPreInit(PinName sda, PinName scl) : I2C(sda, scl) {
-    frequency(400000);
-    start();
-  };
-};
-
-I2CPreInit gI2C(PB_7, PB_6);  //Ici j'utilise i2C1 sur un STM32L432
-Adafruit_SSD1306_I2c gOled(gI2C, PA_7);   //Fake pin not connected
-
+DS18B20 sensor(PA_5);   
 
 int main() {
+  bool rety;
+  int tickTime = 1000;
+  oled.begin(SSD1306_SWITCHCAPVCC);
+  HAL_Delay(200);
+  oled.splash();
+  oled.display();
   HAL_Delay(2000);
-  gOled.clearDisplay();
-  int xpos = 0;
-  int ypos = 0;
+  oled.clearDisplay();
 
   while (1) {
-    
+    myLed = !myLed;
+    float temperature = sensor.readTemp();
 
-    gOled.setTextSize(2);         // Active in config file - Full pixel for car is 15 x 10
-    for (int i = 0; i < 255; i++) {
-      gOled.setTextCursor(xpos, ypos);
-      gOled.writeChar(i + 48);
-      gOled.display();
-      xpos = xpos + 11;   // 1 pixel space
-      if (xpos > 128-10) {   // For clean return
-        ypos = ypos + 16; // 1 pixel space
-        xpos = 0;
-      }
-      if (ypos > 64-15) { // For clean bottom
-        ypos = 0;
-        gOled.clearDisplay();
-      }
-      HAL_Delay(75);
-    }
+    oled.setTextSize(2);
+    oled.setTextCursor(0, 30);
+    oled.printf("Temp: %0.1f\r", temperature);
+    oled.display();
+
+    HAL_Delay(2000);
   }
 }
